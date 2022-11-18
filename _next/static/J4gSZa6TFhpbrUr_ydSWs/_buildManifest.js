f666a4fc4443763ebd2ffb694d99d18ff24e21ec
Weliton Sousa <welitonubuntu@gmail.com>
2022-11-18 01:25:44 -0300

    app using next

---
+++ _next/static/J4gSZa6TFhpbrUr_ydSWs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-710377d328550255.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
